8a9692c46cdb9e0fbb419559c1df3310a0f432ff
Yoseph <yosephten@gmail.com>
2022-03-17 13:45:06 +0300

    Added CORS

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const authorizeKey = require('./utils/authorizeKey');
 const morganBody = require('morgan-body');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express(); // Start the Server
 dotenv.config(); // Configure to access .env files
 mongoose.connect(process.env.DB_URL).catch(error => console.error(error)) // Connect to the Database
+app.use(cors()); // Enable CORS
 app.use(authorizeKey); // Verify API Key in header
 app.use(bodyParser.json()); // Parsing JSON body
 morganBody(app, {noColors: true, stream: logger}); // Initialize logging each api request/response
