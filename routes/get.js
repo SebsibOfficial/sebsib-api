4a2aeedc5c269ce06e7757df9e0af19ce3bb4253
Yoseph <yosephten@gmail.com>
2022-03-21 16:54:53 +0300

    Added JWT tokens

--- routes/get.js
+++ routes/get.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const authorizeToken = require('../utils/authorizeToken');
 const { getMemberController, getMemberListController, getSurveyListController, getProjectListController, getRecentSurveyController, getSurveyController } = require('../controllers');
 
-router.get('/memberlist', getMemberListController);
+router.get('/memberlist', authorizeToken, getMemberListController);
 router.get('/surveylist', getSurveyListController);
 router.get('/projectlist', getProjectListController);
 router.get('/member/:id', getMemberController)
