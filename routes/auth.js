ee93030472e564a9a3aaf6822539c7be1eb4cca2
Yoseph <yosephten@gmail.com>
2022-03-21 16:55:25 +0300

    Finished /login Endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const {User} = require('../models');
 
-router.post('/authorize', (req, res) => {
-  res.json({message: "Hey from Auth"})
+router.post('/login', async (req, res) => {
+  var {email, password} = req.body;
+  if (email != undefined && password != undefined && validator.isEmail(email)) {
+    try {
+      const user = await User.findOne({email: email});
+      if (user != null) {
+        bcrypt.compare(password, user.password, function(err, result) {
+          if (result) {
+            const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+            res.status(200).json({token: token});
+          } else res.status(401).json({message: "Wrong username or password"})
+        });
+      } else res.status(401).json({message: "Wrong username or password"})
+    } catch (error) { res.status(500).send(error) }
+  } else {
+    res.status(400).json({message: "Bad Input"})
+  }
 })
 
 router.post('/fillsettings', (req, res) => {
