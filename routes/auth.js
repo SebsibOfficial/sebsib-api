f753e0d2f146c415ef4bdbe3adc4a5a4db639f8a
Yoseph <yosephten@gmail.com>
2022-03-22 15:41:00 +0300

    Added /fillseting endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
-const {User} = require('../models');
+const {User, Organization} = require('../models');
 
 router.post('/login', async (req, res) => {
   var {email, password} = req.body;
@@ -14,16 +14,39 @@ router.post('/login', async (req, res) => {
           if (result) {
             const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
             res.status(200).json({token: token});
-          } else res.status(401).json({message: "Wrong username or password"})
+          } else res.status(401).json({message: "Wrong credentials"})
         });
-      } else res.status(401).json({message: "Wrong username or password"})
+      } else res.status(401).json({message: "Wrong credentials"})
     } catch (error) { res.status(500).send(error) }
   } else {
     res.status(400).json({message: "Bad Input"})
   }
 })
 
-router.post('/fillsettings', (req, res) => {
-  res.json({message: "Hey from fill settings"})
+
+router.post('/fillsettings', async (req, res) => {
+  var {email, password, org} = req.body;
+  try {
+    var org_obj = await Organization.findOne({name: org});
+    var org_id = org_obj._id;
+  } catch (error) {
+    return res.status(401).json({message: "Wrong credentials"});
+  }
+  if (email != undefined && password != undefined && validator.isEmail(email)) {
+    try {
+      const user = await User.findOne({email: email, organizationid: org_id});
+      if (user != null) {
+        bcrypt.compare(password, user.password, function(err, result) {
+          if (result) {
+            const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+            res.status(200).json({token: token});
+          } else res.status(401).json({message: "Wrong credentials"})
+        });
+      } else res.status(401).json({message: "Wrong credentials"})
+    } catch (error) { res.status(500).send(error) }
+  } else {
+    res.status(400).json({message: "Bad Input"})
+  }
 })
+
 module.exports = router;
