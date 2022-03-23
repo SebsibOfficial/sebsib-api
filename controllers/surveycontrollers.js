af744d1871f13d2f61ab4ccae65093cdf8c3e333
Yoseph <yosephten@gmail.com>
2022-03-23 15:53:34 +0300

    Added /surveylist endpoint

--- controllers/surveycontrollers.js
+++ controllers/surveycontrollers.js
@@ -1,9 +1,21 @@
+const { Project } = require("../models");
+
 const createSurveyController = (req, res) => {
   res.json({message: "Hey from createSurveyController"})
 }
 
-const getSurveyListController = (req, res) => {
-  res.json({message: "Hey from getSurveyListController"})
+const getSurveyListController = async (req, res) => {
+  var projectId = req.params.projectId;
+  try {
+    var projects = await Project.aggregate([
+      {$lookup: { from: 'surveys', localField: 'surveysId', foreignField: '_id', as: 'survey_docs'}}
+    ]);
+    var surveys = projects.filter(project => project._id == projectId);
+    if (surveys.length == 0) return res.status(403).json({message: 'Bad Input'});
+    res.status(200).send(surveys[0].survey_docs);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const getSurveyController = (req, res) => {
