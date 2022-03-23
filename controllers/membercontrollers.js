f12d93cbaa16bd48a1ba9e8475593bcf9e0256a2
Yoseph <yosephten@gmail.com>
2022-03-23 15:53:12 +0300

    Added /memberlist endpoint

--- controllers/membercontrollers.js
+++ controllers/membercontrollers.js
@@ -1,5 +1,15 @@
-const getMemberListController = (req, res, next) => {
-  res.json({message: "Hey from getMemberListController"})
+const { User } = require("../models");
+
+const getMemberListController = async (req, res, next) => {
+  var orgId = req.params.orgId;
+  if (orgId == null) return res.status(403).json({message: 'Bad Input'})
+  try {
+    const memberlist = await User.find({organizationId: orgId})
+    res.status(200).send(memberlist)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Server Error'})
+  }
 }
 
 const createMemberController = (req, res, next) => {
