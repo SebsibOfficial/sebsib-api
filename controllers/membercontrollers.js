61bf077f4fa8b0f1e55930dea5a82616ef7ae1bf
Yoseph <yosephten@gmail.com>
2022-03-24 11:38:30 +0300

    Added /member endpoint

--- controllers/membercontrollers.js
+++ controllers/membercontrollers.js
@@ -16,8 +16,15 @@ const createMemberController = (req, res, next) => {
   res.json({message: "Hey from createMemberController"})
 }
 
-const getMemberController = (req, res, next) => {
-  res.json({message: "Hey from getMemberController"})
+const getMemberController = async (req, res, next) => {
+  var id = req.params.id;
+  try {
+    var member = await User.findOne({_id: id});
+    res.status(200).json(member);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Server Error'});
+  }
 }
 
 const editMemberController = (req, res, next) => {
