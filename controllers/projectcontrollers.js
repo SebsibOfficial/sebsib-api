b7ad18cc7402f1ec96d17e68d817fcfa11f8d6fb
Yoseph <yosephten@gmail.com>
2022-03-24 11:13:13 +0300

    Added /projectlist endpoint

--- controllers/projectcontrollers.js
+++ controllers/projectcontrollers.js
@@ -1,9 +1,22 @@
+const { Organization } = require("../models");
+
 const createProjectController = (req, res) => {
   res.json({message: "Hey from createProjectController"})
 }
 
-const getProjectListController = (req, res) => {
-  res.json({message: "Hey from getProjectListController"})
+const getProjectListController = async (req, res) => {
+  var orgId = req.params.orgId;
+  try {
+    var Orgs = await Organization.aggregate([
+      {$lookup: { from: 'projects', localField: 'projectsId', foreignField: '_id', as: 'project_docs'}}
+    ]);
+    Orgs = Orgs.filter(org => org._id == orgId);
+    if (Orgs.length == 0) return res.status(403).json({message: 'Bad Input'});
+    res.status(200).send(Orgs[0].project_docs);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Server Error'});
+  }
 }
 
 module.exports = {
