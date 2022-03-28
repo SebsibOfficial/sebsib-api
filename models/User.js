49f15d148e424cc0526b4d9b129200359fa800c8
Yoseph <yosephten@gmail.com>
2022-03-28 15:20:08 +0300

    `username` added to Model

--- models/User.js
+++ models/User.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  username: {
+    type: String,
+    default: null
+  },
   password: {
     type: String,
     required: true,
