1f14aec0c791d3e2614d62c5da7f0c95d489a890
Yoseph <yosephten@gmail.com>
2022-03-31 16:40:17 +0300

    Added access control and routes

--- routes/delete.js
+++ routes/delete.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const {deleteMemberController, deleteProjectController, deleteSurveyController} = require('../controllers')
-router.delete('/member/:id', deleteMemberController)
-router.delete('/project/:id', deleteProjectController)
-router.delete('/survey/:id', deleteSurveyController)
+const {deleteMemberController, deleteProjectController, deleteSurveyController} = require('../controllers');
+const accessControl = require('../utils/accessControl');
+
+router.delete('/member/:id', accessControl(3), deleteMemberController)
+router.delete('/project/:id', accessControl(3), deleteProjectController)
+router.delete('/survey/:pid/:sid',accessControl(3), deleteSurveyController)
 
 module.exports = router;
