46e51cff9f4e3b50b073625962eb1851b3861053
Yoseph <yosephten@gmail.com>
2022-04-06 15:58:29 +0300

    Added /editsettings endpoint

--- controllers/settingcontrollers.js
+++ controllers/settingcontrollers.js
@@ -1,5 +1,40 @@
-const editSettingsController = (req, res) => {
-  res.json({message: "Hey from editSettingsController"})
+const { Organization, User } = require("../models");
+const jwt = require('jsonwebtoken');
+const validator = require("validator");
+const bcrypt = require('bcrypt');
+
+const editSettingsController = async (req, res) => {
+  var orgId = req.params.orgId;
+  var userID = jwt.verify(req.header('auth-token'), process.env.TOKEN_SECRET)._id;
+  var {name, email, Opassword, Npassword} = req.body;
+  var user = await User.findById(userID);
+
+  // Check if string is an email
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({message: 'Invalid Email'});
+  }
+  // Check if username exists
+  if (await Organization.exists({name: name, _id: { $ne: orgId}}) || await User.exists({email: email, _id: { $ne: userID}})) {
+    return res.status(400).json({message: 'Name or Email Exists'});
+  }
+  // Check if last password is okay
+  bcrypt.compare(Opassword, user.password, async function(err, result) {
+    if (result) {
+      // Encrypt password
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(Npassword, salt);
+      // Edit Organization
+      var ro = await Organization.updateOne({_id: orgId},{
+        name: name,
+      });
+      // Edit the Owner
+      var ru = await User.updateOne({_id: userID},{
+        email: email,
+        password: hash,
+      });
+      res.status(200).send({ro, ru});
+    } else res.status(401).json({message: "Wrong password"})
+  })
 }
 
 module.exports = {
--- routes/patch.js
+++ routes/patch.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { editSettingsController, editMemberController } = require('../controllers');
 
-router.patch('/editsettings', editSettingsController);
+router.patch('/editsettings/:orgId', editSettingsController);
 router.patch('/editmember/:id', editMemberController)
 
 module.exports = router;
