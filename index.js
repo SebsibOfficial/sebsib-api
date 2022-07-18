d61eb5bdd20669ec53558e84e81cbb4120d44f33
Yoseph <yosephten@gmail.com>
2022-07-18 22:45:48 +0300

    DDOS prevention

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const authorizeKey = require('./utils/authorizeKey');
 const authorizeToken = require('./utils/authorizeToken');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 const app = express(); // Start the Server
 dotenv.config(); // Configure to access .env files
 mongoose.connect(process.env.NODE_ENV == 'dev' ? process.env.TEST_DB_URL : process.env.PROD_DB_URL)
 .catch(error => console.error(error))
 .then(() => app.listen(process.env.PORT, () => console.log("Server Running at "+process.env.PORT+"..."))) // Connect to the Database
+app.use(limiter); // Limit requests
 app.use(cors()); // Enable CORS
 app.use(authorizeKey); // Verify API Key in header
 app.use(bodyParser.json()); // Parsing JSON body
