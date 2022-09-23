d2f556fd03993f7590d9c9fd99a427cc6d6cef5f
Yoseph <yosephten@gmail.com>
2022-09-23 11:04:38 +0300

    id translation clean up

--- utils/translateIds.js
+++ utils/translateIds.js
@@ -1,49 +1,19 @@
+var TYPES = require('./id-text.json');
+
 module.exports = (from, inp) => {
   if (from == 'id') {
-    switch (inp) {
-      case "624558d1a263f17689cdc5bc":
-        return "TEXT";
-      case "624558d1a263f17689cdc5bd":
-        return "CHOICE";
-      case "624558d1a263f17689cdc5be":
-        return "MULTI-SELECT";
-      case "623cc24a8b7ab06011bd1e5f":
-        return "MEMBER";
-      case "623cc24a8b7ab06011bd1e60":
-        return "OWNER";
-      case "623cc24a8b7ab06011bd1e61":
-        return "ADMIN";
-      case "623cc24a8b7ab06011bd1e62":
-        return "SUPER";
-      case "623d73a051e8bcb894b3f7df":
-        return "FREE TRAIL";
-      case "623d73a051e8bcb894b3f7e0":
-        return "UNLIMITED";      
-      default:
-        return false;
+    for (let index = 0; index < TYPES.length; index++) {
+      const element = TYPES[index];
+      if (element._id == inp){
+        return element.text;
+      }
     }
   } else {
-    switch (inp) {
-      case "TEXT":
-        return "624558d1a263f17689cdc5bc";
-      case "CHOICE":
-        return "624558d1a263f17689cdc5bd";
-      case "MULTI-SELECT":
-        return "624558d1a263f17689cdc5be";
-      case "MEMBER":
-        return "623cc24a8b7ab06011bd1e5f";
-      case "OWNER":
-        return "623cc24a8b7ab06011bd1e60";
-      case "ADMIN":
-        return "623cc24a8b7ab06011bd1e61";
-      case "SUPER":
-        return "623cc24a8b7ab06011bd1e62";
-      case "FREE TRAIL":
-        return "623d73a051e8bcb894b3f7df";
-      case "UNLIMITED":
-        return "623d73a051e8bcb894b3f7e0";      
-      default:
-        return false;
+    for (let index = 0; index < TYPES.length; index++) {
+      const element = TYPES[index];
+      if (element.text == inp){
+        return element._id;
+      }
     }
   }
 }
