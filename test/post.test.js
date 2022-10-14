cd9fdfb6b32acd2ebed35d788fa397f5130ce6c0
Yoseph <yosephten@gmail.com>
2022-10-14 11:07:22 +0300

    /sendrequest test impl.

--- test/post.test.js
+++ test/post.test.js
@@ -2,11 +2,17 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
 const server = "http://localhost:3000";
-const {User} = require('../models');
+const ObjectId = require('mongoose').Types.ObjectId;
+const mongoose = require('mongoose');
+const {User, Request} = require('../models');
 chai.use(chaiHttp);
 
 describe('/post/createmember', function () {
     let token = '';
+    // Connect to DB before doing tests
+    before('connect', function(){
+      return mongoose.connect('mongodb://localhost/sebsib')
+    })
     // This executes before every test
     this.beforeEach('Giving Token for Tests', (done) => {
         chai.request(server)
@@ -118,10 +124,64 @@ describe('/post/createmember', function () {
         })
     })
 
-    after((done) => {
-        User.findOneAndDelete({'email':'newmember@to.add'})
-        done()
+    it('Tear Down', async () => {
+      this.timeout(0)
+      User.findOneAndDelete({email:'newmember@to.add'}).catch((err) => {
+        console.log(err)
+      })
     })
+})
+
+describe('/noauth/sendrequest', function () {
+  // Connect to DB before doing tests
+  before('connect', function(){
+    return mongoose.connect('mongodb://localhost/sebsib')
+  })
+
+  it('Sends proper register request', (done) => {
+    chai.request(server)
+    .post('/noauth/sendrequest/REGISTER')
+    .send({
+      "pkg": "FREE TRAIL",
+      "firstname": "firsttest",
+      "lastname": "lasttest",
+      "email": "testemail",
+      "phone": "0912345678",
+      "orgname": "testorgan"
+    })
+    .end((err, res) => {
+      res.should.have.status(200)
+      done()
+    })
+  })
+
+  // Send renew request properly to Database
+  it('Send proper renew request', () => {
+    chai.request(server)
+    .post('/noauth/sendrequest/RENEW')
+    .send({
+      "pkg": "STANDARD",
+      "firstname": "firsttest",
+      "lastname": "lasttest",
+      "email": "testemail",
+      "phone": "0912345678",
+      "orgname": "testorgan",
+      "bank": "DASHEN",
+      "transno": "987IUYJHG3WEDT",
+      "orgId": "FRORSA65"
+    })
+    .end((err, res) => {
+      res.should.have.status(200)
+      done()
+    })
+  })
+
+  it('Tear Down', () => {
+    this.timeout(0);
+    Request.deleteMany({email: 'testemail'}).catch((err) => {
+      console.log(err)
+    })
+  })
 })
 
 describe('/post/createsurvey', () => {
