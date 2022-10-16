79f30c0603629160d2d7a243c3573cba7b5fee06
Yoseph <yosephten@gmail.com>
2022-10-16 15:17:15 +0300

    Testing fixes

--- test/get.test.js
+++ test/get.test.js
@@ -7,23 +7,11 @@ const mongoose = require('mongoose');
 const {User, Request} = require('../models');
 chai.use(chaiHttp);
 
-describe("/get/orgstatus/:shortorgId", function () {
-  // This executes before tests
-  this.beforeAll('Giving Token for Tests', (done) => {
-    chai.request(server)
-    .post('/auth/login')
-    .send({email: process.env.TEST_EMAIL, password: process.env.TEST_PASS})
-    .end((err, res) => {
-      res.should.have.status(200);
-      token = res.body.token;
-      done();
-    })
-  })
+describe("/noauth/orgstatus/:shortorgId", function () {
   // Has data
   it('Has required data', (done) => {
     chai.request(server)
-    .get('/get/orgstatus/FRORSA65')
-    .set('Authorization', token)
+    .get('/noauth/orgstatus/FRORSA65')
     .end((err, res) => {
       res.should.have.status(200)
       done()
