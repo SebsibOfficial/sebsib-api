58a53190b38a79623933a7df83c2a7f0bc4c1a4d
y-ohanne-s <yt61312@gmail.com>
2023-02-06 23:21:53 +0300

    update endpoints

--- models/User.js
+++ models/User.js
@@ -36,6 +36,10 @@ const userSchema = new mongoose.Schema({
     required: false,
     default: ''
   },
+  hasPassChange: {
+    type: Boolean,
+    required: true
+  },
   password: {
     type: String,
     required: true,
